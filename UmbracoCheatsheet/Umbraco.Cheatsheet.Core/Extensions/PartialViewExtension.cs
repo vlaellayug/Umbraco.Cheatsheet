1accf21a00a76d0c4ce13be97ea1f12b3ef659cc
vlaellayug <vlaellayug@yahoo.com.ph>
2022-01-15 22:51:23 +0800

    implement data type variants

---
+++ UmbracoCheatsheet/Umbraco.Cheatsheet.Core/Extensions/PartialViewExtension.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Web.Mvc;
+
+namespace Umbraco.Cheatsheet.Core.Extensions
+{
+    public static class PartialViewExtension
+    {
+        public static void RenderElement<T>(this UmbracoViewPage<T> page, IPublishedElement element, string folder = null, ViewDataDictionary viewData = null)
+        {
+            if (element != null)
+            {
+                var folderString = folder == null ? "" : folder + "/";
+                var partialView = $"{folderString}{element.ContentType.Alias}";
+                page.Html.RenderPartial(partialView, element, viewData);
+            }
+        }
+
+        public static void RenderElements<T>(this UmbracoViewPage<T> page, IEnumerable<IPublishedElement> elements, string folder = null)
+        {
+            if (elements == null || !elements.Any())
+            {
+                return;
+            }
+            foreach (var element in elements)
+            {
+                page.RenderElement(element, folder, new ViewDataDictionary { { "Elements", elements }, { "ViewModel", page.Model } });
+            }
+        }
+    }
+}
---
+++ UmbracoCheatsheet/Umbraco.Cheatsheet.Core/Extensions/StringHelperExtension.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+
+namespace Umbraco.Cheatsheet.Core.Extensions
+{
+    public static class StringHelperExtension
+    {
+        public static IHtmlString ToReplaceLineBreak(this string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return new HtmlString("");
+
+            text = text
+                .Replace("\r\n", "<br/>")
+                .Replace("\r", "<br/>")
+                .Replace("\n", "<br/>");
+
+            return new HtmlString(text);
+        }
+    }
+}
